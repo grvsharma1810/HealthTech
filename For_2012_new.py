d6148c7021d1078936d2724c4ca6023f41b1caa7
Gaurav Kumar Sharma <gs8280601@gmail.com>
2019-04-07 08:36:51 +0530

    Add files via upload

---
+++ For_2012_new.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+import matplotlib
+import matplotlib.ticker as ticker
+from matplotlib import pyplot as plt
+import csv
+
+data = [row for row in csv.reader(open("t.csv"))]
+l=[]
+name=[]
+for row in data:
+    for col in row :
+        i=data.index(row)
+        j=row.index(col)
+        if i>=1 :
+            if(col=='01.01.2012') :
+                l.append(data[i][j+2])
+                name.append(data[i][1])
+                break
+for i in range(len(l)):
+    l[i]=int(l[i])
+plt.plot(l,name)
+plt.xlabel("Number of beds")
+plt.show()
